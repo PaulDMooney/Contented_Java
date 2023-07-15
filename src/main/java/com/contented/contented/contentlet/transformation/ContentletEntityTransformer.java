12b0c166816acdcfa95fc6fab305d81513128128
Paul Mooney <paul.mooney@live.com>
2023-07-15 12:08:56 -0400

    refactor: Move Predicate in inheritance structure
    A Transformer does not have to be a predicate, so the Predicate inheritance is moved further down the tree.

--- src/main/java/com/contented/contented/contentlet/transformation/ContentletEntityTransformer.java
+++ src/main/java/com/contented/contented/contentlet/transformation/ContentletEntityTransformer.java
@@ -2,5 +2,7 @@ package com.contented.contented.contentlet.transformation;
 
 import com.contented.contented.contentlet.ContentletEntity;
 
-public interface ContentletEntityTransformer extends Transformer<ContentletEntity, ContentletEntity> {
+import java.util.function.Predicate;
+
+public interface ContentletEntityTransformer extends Transformer<ContentletEntity, ContentletEntity>, Predicate<ContentletEntity> {
 }
--- src/main/java/com/contented/contented/contentlet/transformation/Transformer.java
+++ src/main/java/com/contented/contented/contentlet/transformation/Transformer.java
@@ -2,9 +2,7 @@ package com.contented.contented.contentlet.transformation;
 
 import com.contented.contented.contentlet.ContentletEntity;
 
-import java.util.function.Predicate;
-
-public interface Transformer<T, R> extends Predicate<T> {
+public interface Transformer<T, R> {
 
     R transform(T toTransform);
 }
