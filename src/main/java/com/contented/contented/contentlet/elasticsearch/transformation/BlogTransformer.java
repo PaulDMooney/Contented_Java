dcb691620cb395d5ad4292f976471381426bc3aa
Paul Mooney <paul.mooney@live.com>
2023-07-27 09:22:59 -0400

    feat: Initial ES Blog Transformer
    Initial transformer which takes a Blog contentletEntity and transforms it into an EntityMap for ElasticSearch

---
+++ src/main/java/com/contented/contented/contentlet/elasticsearch/transformation/BlogTransformer.java
@@ -0,0 +1,27 @@
+package com.contented.contented.contentlet.elasticsearch.transformation;
+
+import com.contented.contented.contentlet.ContentletEntity;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.elasticsearch.client.elc.EntityAsMap;
+
+import static com.contented.contented.contentlet.transformation.StandardDMSContentTransformer.BLOG_VALUE;
+import static com.contented.contented.contentlet.transformation.StandardDMSContentTransformer.CONTENT_TYPE_FIELD;
+
+public class BlogTransformer implements ESRecordTransformer {
+    @Override
+    public EntityAsMap transform(ContentletEntity toTransform) {
+
+        EntityAsMap entityAsMap = new EntityAsMap();
+        entityAsMap.put("contentType", StringUtils.lowerCase(toTransform.get("contentType")));
+        entityAsMap.put("language", toTransform.get("language"));
+        entityAsMap.put("identifier", toTransform.getId());
+        entityAsMap.put("blog.title", toTransform.get("title"));
+
+        return entityAsMap;
+    }
+
+    @Override
+    public boolean test(ContentletEntity contentletEntity) {
+        return BLOG_VALUE.equals(contentletEntity.getSchemalessData().get(CONTENT_TYPE_FIELD));
+    }
+}
