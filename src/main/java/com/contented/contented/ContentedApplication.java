007f17ef4625ca56668bf2f909e5eedd92533404
Paul Mooney <paul.mooney@live.com>
2024-04-20 16:08:57 -0400

    chore: Exclude unused auto-configurations

--- src/main/java/com/contented/contented/ContentedApplication.java
+++ src/main/java/com/contented/contented/ContentedApplication.java
@@ -3,13 +3,15 @@ package com.contented.contented;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.LogbackMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Hooks;
 
 import java.time.Clock;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {LogbackMetricsAutoConfiguration.class, SqlInitializationAutoConfiguration.class})
 @OpenAPIDefinition(info = @Info(title = "Contented API", version = "1.0", description = "Documentation Contented API v1.0"))
 public class ContentedApplication {
 
